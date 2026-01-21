31e63322d92e978b064fb5e728bce50a89d61041
ViriMares <virimaresoui@gmail.com>
2026-01-21 10:15:01 -0800

    Initial Commit

---
+++ myresumee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myresumee
+//
+//  Created by viri on 1/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myresumee/myresumeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  myresumeeApp.swift
+//  myresumee
+//
+//  Created by viri on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct myresumeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
